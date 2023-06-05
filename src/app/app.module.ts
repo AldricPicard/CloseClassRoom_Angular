617d99202b5956696893f056c895dbafa2c17ac7
Jonathan Rigord <jonathan.rigord@icloud.com>
2023-06-05 15:53:21 +0200

    creation du formulaire d'auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CommunComponent } from './shared/commun/commun.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { BarreDeNavigationComponent } from './shared/components/barre-de-navigation/barre-de-navigation.component';
+import { InscriptionComponent } from './views/inscription/inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommunComponent,
     HeaderComponent,
     FooterComponent,
-    BarreDeNavigationComponent
+    BarreDeNavigationComponent,
+    InscriptionComponent
   ],
   imports: [
     BrowserModule,
