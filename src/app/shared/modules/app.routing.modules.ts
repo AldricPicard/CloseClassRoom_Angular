366fe89ca275f465d0f21d37aa90dcdf34cca360
Mazzitelli Ryan <DevMazzitelli@gmail.com>
2023-06-06 10:22:04 +0200

    Finalisation de la création des routes

--- src/app/shared/modules/app.routing.modules.ts
+++ src/app/shared/modules/app.routing.modules.ts
@@ -1,21 +1,36 @@
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes} from "@angular/router";
-import {LocationGuardService} from "../guards/location-guard.service";
+import { RouterModule, Routes } from '@angular/router';
 import {AccueilComponent} from "../../views/accueil/accueil.component";
 import {InscriptionComponent} from "../../views/inscription/inscription.component";
 import { ConnexionComponent } from "src/app/views/connexion/connexion.component";
+import {MentionsComponent} from "../components/mention/mentions.component";
+import {PolitiqueComponent} from "../components/politique/politique.component";
+import {FormationComponent} from "../../views/formation/formation.component";
+import {AlternanceComponent} from "../../views/alternance/alternance.component";
+import {FinancementComponent} from "../../views/financement/financement.component";
+import {EntrepriseComponent} from "../../views/entreprise/entreprise.component";
 
 
 const routes: Routes = [
-  { path: '', component: AccueilComponent, canActivate: [LocationGuardService] },
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' },
   { path: 'accueil', component: AccueilComponent },
   { path: 'inscription', component: InscriptionComponent},
-  { path: 'connexion', component: ConnexionComponent}
+  { path: 'connexion', component: ConnexionComponent},
+  { path: 'mentions' , component: MentionsComponent},
+  { path: 'politique' , component: PolitiqueComponent},
+  { path: 'formation', component: FormationComponent},
+  { path: 'alternance', component: AlternanceComponent},
+  { path: 'financement', component: FinancementComponent},
+  { path: 'entreprise', component: EntrepriseComponent},
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes),
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModules {
+
+}
