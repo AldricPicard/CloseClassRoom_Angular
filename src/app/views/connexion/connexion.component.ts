50c897627cd31b13f73a2f2e34b55f5241280f73
Mazzitelli Ryan <DevMazzitelli@gmail.com>
2023-06-07 13:39:46 +0200

    Ajout connexion.component.ts

--- src/app/views/connexion/connexion.component.ts
+++ src/app/views/connexion/connexion.component.ts
@@ -2,6 +2,7 @@ import {Component, Inject} from '@angular/core';
 import {Observable} from "rxjs";
 import {User} from "../../shared/models/user";
 import {HttpClient} from "@angular/common/http";
+import {AuthentificationService} from "../../../services/authentification.service";
 
 @Component({
   selector: 'app-connexion',
@@ -10,21 +11,15 @@ import {HttpClient} from "@angular/common/http";
 })
 export class ConnexionComponent {
   public appelAPI$: Observable<User>
-
   constructor(
-    @Inject(HttpClient) private http:HttpClient
-  ) {
-    this.appelAPI$ = this.http.get<User>('https://127.0.0.1:8000/api/users.json')
+    // Injection des dépendances
+    @Inject(AuthentificationService) private authentificationService:AuthentificationService,
+    @Inject(HttpClient) private http:HttpClient) {
+    this.appelAPI$ = this.http.get<User>('https://127.0.0.1:8000/api/utilisateurs.json')
   }
 
+
   connexion(value: any) {
-    // Vérification des données dans la base de données du mail et du mot de passe pour se connecter
-    this.http.post('http://localhost:3000/api/users',
-      {email: value.email, password: value.password}).subscribe((res: any) => {
-        console.log('Vous êtes connecté.');
-      }, (error) => {
-        console.log('Erreur lors de la connexion.');
-      }
-    );
+    this.authentificationService.signIn({email: value.email, password: value.password});
   }
 }
