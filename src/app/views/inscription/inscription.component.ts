9b2dfd3a745a74ec4cb59c5158df0ace6dfd5820
AldricPicard <picard.aldric@gmail.com>
2023-06-08 14:34:46 +0200

    correctif inscription.component.ts

--- src/app/views/inscription/inscription.component.ts
+++ src/app/views/inscription/inscription.component.ts
@@ -3,7 +3,7 @@ import {Observable} from "rxjs";
 import {User} from "../../shared/models/user";
 import {HttpClient} from "@angular/common/http";
 import { DatePipe } from '@angular/common';
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-inscription',
@@ -12,41 +12,42 @@ import {Router} from "@angular/router";
 })
 export class InscriptionComponent {
   public user$!: Observable<User[]>
-
+  loc!: string;
   constructor( // Injection des dépendances
-               @Inject(HttpClient) private http: HttpClient,
-               private datePipe: DatePipe,
-                private router : Router
-               ) {
+    @Inject(HttpClient) private http: HttpClient,
+    private datePipe: DatePipe,
+    private router : Router,
+    private route: ActivatedRoute
+  ) {
     this.user$ = this.http.get<User[]>('https://127.0.0.1:8000/api/users.json')
-    }
 
-    // Méthode pour ajouter un utilisateur.
-    ajouterUtilisateur(value: any) {
-      const now = this.datePipe.transform(new Date(), 'yyyy-MM-dd HH:mm:ss');
-      this.http.post('https://127.0.0.1:8000/api/users',
-        {
-          // Renvoie les sexe de l'utilisateur en fonction de la valeur du bouton radio
-          sexe: value.sexe,
-          firstname: value.firstname,
-          lastname: value.lastname,
-          birth: value.birth,
-          email: value.email,
-          phone: value.phone,
-          password: value.password,
-          address: value.address,
-          city: value.city,
-          department: value.department,
-          postalCode: value.postalCode,
-          createAt : now
-        }
-      ).subscribe(
-        () => {
-          console.log('Utilisateur ajouté');
-          this.router.navigate(['/connexion']);
+  }
 
-
-        }
-      )
-    }
+  // Méthode pour ajouter un utilisateur.
+  ajouterUtilisateur(value: any) {
+    const now = this.datePipe.transform(new Date(), 'yyyy-MM-dd HH:mm:ss');
+    this.http.post('https://127.0.0.1:8000/api/users',
+      {
+        // Renvoie les sexe de l'utilisateur en fonction de la valeur du bouton radio
+        sexe: value.sexe,
+        firstname: value.firstname,
+        lastname: value.lastname,
+        birth: value.birth,
+        email: value.email,
+        phone: value.phone,
+        password: value.password,
+        address: value.address,
+        city: value.city,
+        department: value.department,
+        postalCode: value.postalCode,
+        createAt : now
+      }
+    ).subscribe(
+      () => {
+        console.log('Utilisateur ajouté');
+        this.loc = this.route.snapshot.params['loc'];
+        this.router.navigate([this.loc, 'connexion']);
+      }
+    )
+  }
 }
